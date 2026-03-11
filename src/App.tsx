e2da0c6640127901376d26badb825897decef8ae
RK0519 <ramya.khandiga@gmail.com>
2026-03-11 20:46:17 +0530

    Final commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
@@ -8,8 +8,15 @@ import Generate from "./pages/Generate";
 import MyGeneration from "./pages/MyGeneration";
 import YTPreview from "./pages/YTPreview";
 import Login from "./components/Login";
+import { useEffect } from "react";
 
 export default function App() {
+
+    const {pathname} = useLocation()
+    useEffect(()=>{
+        window.scrollTo(0,0)
+    },[pathname])
+    
     return (
         <>
             <LenisScroll />
@@ -18,7 +25,7 @@ export default function App() {
                 <Route path="/" element={<HomePage />} />
                 <Route path="/generate" element={<Generate />} />
                 <Route path="/generate/:id" element={<Generate />} />
-                <Route path="/my-generate" element={<MyGeneration />} />
+                <Route path="/my-generations" element={<MyGeneration />} />
                 <Route path="/preview" element={<YTPreview />} />
                 <Route path="/login" element={<Login />} />
             </Routes>
